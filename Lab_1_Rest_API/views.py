f1fc7819efa8d8ab2744fc81db6a770b772849f7
anton3808 <antonsadlov@gmail.com>
2022-10-20 17:39:45 +0300

    Create User and Output Users lists

--- Lab_1_Rest_API/views.py
+++ Lab_1_Rest_API/views.py
@@ -1,6 +1,41 @@
 from Lab_1_Rest_API import app
+from flask import jsonify, request
+import datetime
 
 
-@app.route("/")
-def helloWorld():
-    return "Hello World!"
+id_of_user = 1
+
+
+USERS = [
+    {
+        "id": 1,
+        "name": "Anton",
+    }
+]
+
+
+
+
+
+# GET / users
+# POST /user
+
+
+
+@app.route("/users")
+def get_users():
+    return jsonify({"users": USERS})
+
+
+@app.route("/user", methods=['POST'])
+def create_user():
+    global id_of_user
+
+    request_data = request.get_json()
+    id_of_user += 1
+
+    USERS.append({
+        "id": id_of_user,
+        "name": request_data['name']
+    })
+    return jsonify(request_data)
