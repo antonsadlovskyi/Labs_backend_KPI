34b2e3dd255f1c50c2b6419f066319146aa5e0b6
anton3808 <antonsadlov@gmail.com>
2022-10-20 17:48:09 +0300

    Create Category and Output Categories list

--- Lab_1_Rest_API/views.py
+++ Lab_1_Rest_API/views.py
@@ -1,9 +1,11 @@
 from Lab_1_Rest_API import app
 from flask import jsonify, request
-import datetime
+
+
 
 
 id_of_user = 1
+id_of_category = 1
 
 
 USERS = [
@@ -12,16 +14,19 @@ USERS = [
         "name": "Anton",
     }
 ]
+CATEGORIES = [
+    {
+        "id": 1,
+        "name": "Food",
+    }
+]
 
 
 
 
 
-# GET / users
+# GET /users
 # POST /user
-
-
-
 @app.route("/users")
 def get_users():
     return jsonify({"users": USERS})
@@ -39,3 +44,30 @@ def create_user():
         "name": request_data['name']
     })
     return jsonify(request_data)
+
+
+
+
+
+
+
+# GET /categories
+# POST /category
+@app.route("/categories")
+def get_categories():
+    return jsonify({"categories": CATEGORIES})
+
+
+@app.route("/category", methods=['POST'])
+def create_category():
+    global id_of_category
+    id_of_category += 1
+
+    request_data = request.get_json()
+
+    CATEGORIES.append({
+        "id": id_of_category,
+        "name": request_data['name']
+    })
+    return jsonify(request_data)
+
